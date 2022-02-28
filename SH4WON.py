a058a74b287280ca6ee348682703289679d64d32
SH4WON Sheikh <100561768+SHA4ON@users.noreply.github.com>
2022-02-28 19:16:48 +0600

    Create SH4WON.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    pip2 install requests 
    pip2 install mechanize
    git clone https://github.com/SHA4ON//new/main
    
    Cd SH4WON
    
    Python2 SH4WON.py

---
+++ SH4WON.py
@@ -0,0 +1,60 @@
+import os
+
+os.system("clear")
+
+G="\033[1;32m"
+
+B="\033[1;34m"
+
+Y="\033[1;33m"
+
+P="\033[1;35m"
+
+C="\033[1;36m"
+
+design="""
+
+{G}
+
+░█▀▀▀█ ░█─░█ ─█▀█─ ░█──░█ ░█▀▀▀█ ░█▄─░█ 
+
+─▀▀▀▄▄ ░█▀▀█ █▄▄█▄ ░█░█░█ ░█──░█ ░█░█░█ 
+
+░█▄▄▄█ ░█─░█ ───█─ ░█▄▀▄█ ░█▄▄▄█ ░█──▀█
+
+""".format(P=P,C=C,G=G,Y=Y) 
+
+logo=("""{B}
+
+**************************************************
+
+{G}Remember >>  •••Allah Always Watching You•••
+
+{B}**************************************************
+
+{C} ____We Work To Sarve You a Clean Cyber ____
+
+{Y}   ____Well Come To BLACK SQUAD SPAMMING TEAM____
+
+{B}**************************************************
+
+{ascii}
+
+{B}**************************************************
+
+{G}Author   >>  SH4WON Chowdhury 
+
+{Y}Facebook >>  https://facebook.com/sahadat.34
+
+{C}Github   >>  https://github.com/SH4WON
+
+{B}**************************************************
+
+""".format(ascii=design,B=B,C=C,G=G,Y=Y))
+
+User="SH4WON"
+
+Pwd="BD"
+
+Permission="On"
+
